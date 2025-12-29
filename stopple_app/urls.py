640a837d4a00494ba87fcb9cb7f9550df7c723b0
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-12-29 09:29:14 +0100

    First view using a bootstrap template

---
+++ stopple_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from stopple_app import views
+
+
+urlpatterns = [
+    path("", views.index),
+]
--- stopple_web/settings.py
+++ stopple_web/settings.py
@@ -55,13 +55,14 @@ ROOT_URLCONF = "stopple_web.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        # "DIRS": ["stopple_app/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.static",
             ],
         },
     },
